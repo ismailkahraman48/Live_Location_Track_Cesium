236f6b87ff506dd19bd864dc0a73c1dae1e6e932
ismail kahraman <ismail.kahraman@gmail.com>
2026-01-27 20:34:33 +0300

    check

--- ui/src/utils/getApiUrl.ts
+++ ui/src/utils/getApiUrl.ts
@@ -1,7 +1,7 @@
 export const getApiUrl = (path: string) => {
     const envUrl = import.meta.env.VITE_API_URL;
     let baseUrl = envUrl || "http://127.0.0.1:8080";
-
+    console.log(baseUrl, "   baseurl")
     if (baseUrl.endsWith('/')) {
         baseUrl = baseUrl.slice(0, -1);
     }
